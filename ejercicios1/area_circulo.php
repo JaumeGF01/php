440164fb75487b4ded203492aff467af8edb4d70
JaumeGF01 <jaugarfra@alu.edu.gva.es>
2024-09-20 12:33:39 +0200

    area_circulo_v1

---
+++ ejercicios1/area_circulo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Area Círculo</title>
+</head>
+<body>
+    <h3>Area Circulo</h3>
+    <hr>
+    <?php
+        $radio = 3.5;
+        $pi = 3.1416;
+
+        $area = $pi * $radio**2
+    ?>
+
+    <p><?="El área del círculo es $area"?></p>
+</body>
+</html>
