1e1e191bd73cab792bf0c8b641e5e14d7fd6c307
JaumeGF01 <jaugarfra@alu.edu.gva.es>
2024-09-20 12:17:15 +0200

    first commit

---
+++ ejercicios1/info_basica.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Info_Basica</title>
+</head>
+<body>
+    <h3>Información Básica</h3>
+    <hr>
+    <?php
+        $nombre = "Jaume";
+        $anyo = 2001;
+    ?>
+
+    <p><?="Me llamo $nombre y nací en el año $anyo"?></p>
+</body>
+</html>
