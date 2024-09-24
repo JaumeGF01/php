f10442f47729d21cc2d4c9fbddd6ac4f4f22de8d
JaumeGF01 <jaugarfra@alu.edu.gva.es>
2024-09-24 21:36:45 +0200

    ficheros_v2

--- ficheros/plantillas.php
+++ ficheros/plantillas.php
@@ -1,47 +1,27 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <title>Intercambia</title>
-    <style>
-        table,tr,td,th{
-            border-collapse:collapse;
-            border: 1px solid black;
-            padding: 3px;
-        }
-    </style>
-</head>
-<body>
-    <h3>Plantillas</h3>
-    <hr>
-    <?php
-        $lista = [];
-        $listaFinal =[];
-        $plantillas = fopen("plantillas.csv","r");
-        while (!feof($plantillas)) {
-            $linea = explode(",",fgets($plantillas));
-            $lista[] = $linea;
-        }
-        // echo "<table>";
-        $cabecera = array_shift($lista);
+<?php
+    $lista = [];
+    $plantillas = fopen("plantillas.csv","r");
+    while (!feof($plantillas)) {
+        $linea = explode(",",fgets($plantillas));
+        $lista[] = $linea;
+    }
+    fclose($plantillas);
+    
+    $cabecera = array_shift($lista);
+
+    foreach ($lista as $valor) {
+        $claves = array_combine($cabecera, $valor);
+        $new[] = $claves;
+    }
+    echo "<table>";
+    foreach ($new as $list) {
         
-        foreach ($lista as $fila) {
-            
-            for ($i=0; $i < count($lista); $i++) { 
-                echo $fila[$i];
-                
-            }
+        if ($claves["Equipo"] == "Atlético de Madrid") {
+            echo "<tr>";
+            echo "<td>{$list["Dorsal"]} {$list["Nombre"]} {$list["Apellidos"]} {$list["Posicion"]} {$list["Equipo"]}</td>";
+            echo "</tr>";
         }
-        // for ($i=0; $i < count($lista); $i++) {
-        //     echo "<tr>";
-        //     for ($c=0; $c < count($linea); $c++) {
-        //         echo "<td>{$lista[$i][$c]}</td>";
-        //     }
-        //     echo "</tr>";
-        // }echo "<br>";
-        // echo "</table>";
+    }echo "</table>";
+    
 
-        fclose($plantillas);
-
-    ?>
-</body>
-</html>
+?>
---
+++ ficheros/plantillas.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Plantillas</title>
+        <style>
+            table,tr,td,th{
+                border-collapse:collapse;
+                border: 1px solid black;
+                padding: 3px;
+            }
+        </style>
+    </head>
+    <body>
+        <h3>Plantillas</h3>
+        <hr>
+    </body>
+</html>
