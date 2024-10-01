cdf9dbce417541c5b04e794492395cc7acabcd7d
JaumeGF01 <jaugarfra@alu.edu.gva.es>
2024-10-01 14:42:22 +0200

    formularios_v2

--- formularios/calculadora.php
+++ formularios/calculadora.php
@@ -1,13 +1,10 @@
 <?php
-    //print_r($_GET);
     $x = $_GET["x"];
     $y = $_GET["y"];
     $suma = $x + $y;
     $resta = $x - $y;
     $div = $x / $y;
     $multi = $x * $y;
-
-    //echo "Suma: $suma Resta: $resta Multiplicación: $multi División: $div";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,36 +12,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        table,tr,td,th{
-            border-collapse:collapse;
-            border: 1px solid black;
-            padding: 3px;
+        .table{
+            text-align: center;
+            width: 30vh;
+            margin: auto;
         }
-        table{
-            margin:auto;
-        }
-        
     </style>
 </head>
 <body>
     <?=
-    "<table>
+    "<table class='table table-dark table-striped-columns'>
+    <tr class='table-success'>
+        <th>Valores</th>
+        <th>x=$x y=$y</th>
+    </tr>
     <tr>
         <td>Suma</td>
-        <td>$x + $y = $suma</td>
+        <td>$suma</td>
     </tr>
     <tr>
         <td>Resta</td>
-        <td>$x - $y = $resta</td>
+        <td>$resta</td>
     </tr>
     <tr>
         <td>Multiplicación</td>
-        <td>$x * $y = $multi</td>
+        <td>$multi</td>
     </tr>
     <tr>
         <td>Division</td>
-        <td>$x / $y = $div</td>
+        <td>$div</td>
     </tr>
     </table>"
     ?>
